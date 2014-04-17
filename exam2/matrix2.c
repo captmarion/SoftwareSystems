f13b4e43fecf6e6a463608f016ba2df062925e98
Ken Berry <capt.marion@gmail.com>
2014-04-17 02:35:02 -0400

    adding updated version of matrix2.c for exam 2

--- exam2/matrix2.c
+++ exam2/matrix2.c
@@ -3,6 +3,8 @@
 Copyright 2014 Allen Downey
 License: Creative Commons Attribution-ShareAlike 3.0
 
+Updated by Ken Berry for SoftSys Exam 2
+
 */
 
 
@@ -155,6 +157,23 @@ double *row_sum(Matrix *A) {
     return res;
 }
 
+// Adds up the columns of A and returns a heap-allocated array of doubles.
+double *col_sum(Matrix *A) {
+    double total;
+    int i, j;
+
+    double *res = malloc(A->cols * sizeof(double));
+
+    for (j=0; j<A->cols; j++) {
+	total = 0.0;
+	for (i=0; i<A->rows; i++) {
+	    total += A->data[i][j];
+	}
+	res[j] = total;
+    }
+    return res;
+}
+
 /* 
    http://en.wikipedia.org/wiki/Magic_square
 
@@ -169,9 +188,80 @@ double *row_sum(Matrix *A) {
    Feel free to use row_sum().
 */
 
+int is_magic_square(Matrix *A) {
+	int i, j, f, b;
+	double f_sum = 0.0;
+	double b_sum = 0.0;
+
+	/* Test 1: Is matrix square? */
+	if (A->rows != A->cols)
+	{
+		printf("Heck, this matrix ain't even square!\n");
+		return 0;
+	}
+	
+	/* Test 2: Do rows sum properly? */
+    double *r_sums = row_sum(A);
+	double test_val = r_sums[0];
+
+	for (i=0; i<A->rows; i++)
+	{
+		if (r_sums[i] != test_val)
+		{
+			printf("Not magic! Row %d don't add up right.\n", i);
+			return 0;
+		}
+
+		else continue;
+	}
+	
+	/* Test 3: Do columns sum properly? */
+    double *c_sums = col_sum(A);
+
+	for (j=0; j<A->cols; j++)
+	{
+		if (c_sums[j] != test_val)
+		{
+			printf("Not magic! Column %d don't add up right.\n", j);
+			return 0;
+		}
+
+		else continue;
+	}
+
+	/* Test 4: Does forward diagonal sum properly? */
+	for (f=0; f<A->rows; f++)
+	{
+		f_sum += A->data[f][f];
+	}
+
+	if (f_sum != test_val)
+	{
+	printf("Not magic! Forward diagonal don't add up right.\n");
+	return 0;
+	}
+
+	
+	/* Test 5: Does backwards diagonal sum properly? */
+	for (b=0; b<A->rows; b++)
+	{
+		b_sum += A->data[(A->rows - 1 - b)][b];
+	}
+
+	if (b_sum != test_val)
+	{
+	printf("Not magic! Backwards diagonal don't add up right.\n");
+	return 0;
+	}
+
+
+	printf("Yessir, this here matrix is indeed magic!\n");
+	return 1;
+}
+
 
 int main() {
-    int i;
+    int i, j;
 
     Matrix *A = make_matrix(3, 4);
     consecutive_matrix(A);
@@ -202,6 +292,24 @@ int main() {
 	printf("row %d\t%lf\n", i, sums[i]);
     }
     // should print 6, 22, 38
+   
+	double *c_sums = col_sum(A);
+    for (j=0; j<A->cols; j++) {
+	printf("col %d\t%lf\n", j, c_sums[j]);
+    }
+    // should print 12, 15, 18, 21
+
+	is_magic_square(A);
+
+	double r1[3] = {8,1,6};
+	double r2[3] = {3,5,7};
+	double r3[3] = {4,9,2};
+	double * m[3] = {r1, r2, r3};
+
+	Matrix *magic = make_matrix( 3, 3 );
+		magic->data =  m;
+
+	is_magic_square(magic);
 
     return 0;
 }
